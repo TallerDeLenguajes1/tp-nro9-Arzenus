a7606f8798563fe2b1959b3bfb6a19a8688268ab
Arzenus <nicolastarulli@hotmail.com>
2019-05-30 19:31:34 -0300

    Funciones Binarias

---
+++ Tp 9/Tp 9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+using System.IO;
+
+namespace Tp_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Taller 1\Repositorios Git\tp-nro9-Arzenus\Tp 9\Tp 9\bin\Debug";
+
+            string[] archivos = Directory.GetFiles(path);
+
+            for (int i = 0; i < archivos.Length; i++)
+            {
+                Console.Write(Path.GetFileName(archivos[i]));
+                Console.Write("\n");
+            }
+
+            for (int i = 0; i < archivos.Length; i++)
+            {
+                if (Path.GetExtension(archivos[i]) == ".exe")
+                {
+                    Console.Write("Es exe\n");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tp 9/Tp 9/SoporteParaConfiguracion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Helpers
+{
+    public static class SoporteParaConfiguracion
+    {
+        public static void CrearArchivoDeConfiguracion()
+        {
+            BinaryWriter escritorBinario;
+
+            string ubicacion = @"C:\Prueba_De_Binario";
+
+            escritorBinario = new BinaryWriter(new FileStream("Localizacion.dat", FileMode.Create));
+
+            escritorBinario.Write(ubicacion);
+
+            escritorBinario.Close();
+        }
+
+        public static string LeerConfiguracion()
+        {
+            BinaryReader lectorBinario;
+
+            lectorBinario = new BinaryReader(new FileStream("Localizacion.dat", FileMode.Open));
+
+            string localizacion = lectorBinario.ReadString();
+
+            lectorBinario.Close();
+
+            return localizacion;
+            
+           
+        }
+    }
+}
